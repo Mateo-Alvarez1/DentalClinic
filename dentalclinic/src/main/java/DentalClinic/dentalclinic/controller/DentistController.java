f357b67ae8fecdbd38e7c02e14344b08b6152414
Erick Romero <erickjhorman@gmail.com>
2023-08-30 10:00:22 -0500

    feat: tips about declaring controllers and rest urls

--- dentalclinic/src/main/java/DentalClinic/dentalclinic/controller/DentistController.java
+++ dentalclinic/src/main/java/DentalClinic/dentalclinic/controller/DentistController.java
@@ -16,43 +16,49 @@ import java.util.Collection;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/dentists")
+@RequestMapping("api/v1/dentists") //This is a good way of creating your url
 public class DentistController {
 
     private final PatientService patientService;
     private final DentistService dentistService;
     private final AppointmentService appointmentService;
 
-
     //DENTIST
-    @PostMapping("/create")
+    /* @PostMapping("/create") you shouldn't use actions to describe your intention, use http verbs instead,
+    rest works according http states and verbs
+    */
+    @PostMapping() //here your using http post to persist a resource
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<String> createDentist(@RequestBody DentistDto dentistDto) throws ObjectAlreadyExistException {
         dentistService.createDentist(dentistDto);
         return ResponseEntity.ok("Dentist " + dentistDto.getFirstname() + " successfully persist");
     }
 
-    @PutMapping("/update/{id}")
+    //@PutMapping("/update/{id}")
+    @PutMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<String> updateDentist(@RequestBody DentistDto dentistDto, @PathVariable Long id) throws ResourceNotFoundException {
         dentistService.updateDentist(dentistDto , id);
         return ResponseEntity.ok().body("Dentist " + id + " successfully updated");
     }
 
-    @DeleteMapping("/delete/{id}")
+    //@DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<String> deleteDentist(@PathVariable Long id) throws ResourceNotFoundException {
         dentistService.deleteDentist(id);
         return ResponseEntity.ok().body("Dentist " + id + " successfully deleted");
     }
 
-    @GetMapping("/getDentist/{id}")
+    //@GetMapping("/getDentist/{id}")
+    @GetMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Dentist> getDentist(@PathVariable Long id) throws ResourceNotFoundException {
         return ResponseEntity.ok(dentistService.getDentist(id));
     }
 
-    @GetMapping("/getAll")
+    //@GetMapping("/getAll")
+    @GetMapping()
     @PreAuthorize("hasRole('ROLE_ADMIN' , 'ROLE_USER')")
     public ResponseEntity<Collection<DentistDto>> getAllDentist() throws ResourceNotFoundException {
         return ResponseEntity.ok().body(dentistService.getAllDentists());
@@ -60,14 +66,19 @@ public class DentistController {
 
 
     //APPOINTMENT
-    @GetMapping("/appointments/getByDentist")
+    /*
+    here you need to get dentist's appointments, so a better way of declaring this route would be something like
+    @GetMapping("/appointments")  here your're getting all appointments of a specific dentist
+     */
+    @GetMapping("/{dentistId}/appointments/getByDentist")
     @PreAuthorize("hasRole('ROLE_ADMIN' , 'ROLE_USER')")
+    //I think you should return a implementation of data structure more commonly List not Collection because you're doing it very generic
     public ResponseEntity<Collection<AppointmentDto>> findAppointmentByDentist(@RequestBody DentistDto dentistDto) throws ResourceNotFoundException {
         return ResponseEntity.ok().body(appointmentService.findAppointmentByDentist(dentistDto));
     }
 
 
-    @GetMapping("/appointments/getAll")
+    @GetMapping("/appointments/getAll") //The same as above
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Collection<AppointmentDto>> getAllAppointments() throws ResourceNotFoundException {
         return ResponseEntity.ok().body(appointmentService.getAllAppointments());
